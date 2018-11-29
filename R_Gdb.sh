087621745e0186ffe8bf28c149eafbf70cee453d
rozwel <935419854@qq.com>
2018-11-29 11:14:16 +0800

    Update R_Gdb.sh
    change the function of R_Gdb

--- R_Gdb.sh
+++ R_Gdb.sh
@@ -1,26 +1,44 @@
 function R_Gdb()
 {
-    local filename
-    local flag=0
+    local process_dir=2
+
+    local app
+    local dir
+    local flag
     
-    if [[ $1 == "-d" && $# == 2 ]]
+    if [[ $# == 0 ]]
     then
-        filename=$2
-        flag=1
-    elif [[ $# == 1 ]]
-    then
-        filename=$1
-    else
         return;
     fi
     
-    if [[ -z `pidof $filename` ]]
+    app=$1;
+    if [[ -z `pidof $app` ]]
     then
-        R_Echo Query Fail !
-    elif [[ $flag == 0 ]]
-    then
-        gdb -p `pidof $filename`
+        flag=$program;
     else
-        gdb `find /home/as -type d -printf '-d %p '` -p `pidof $filename`
+        flag=$process;
+        if [[ $# == 3 && $2 == "-d" ]]
+        then
+            flag=$process_dir;
+            dir=$3;
+        fi
     fi
+    
+    case $flag in
+        $program)
+            gdb $app;
+        ;;
+        
+        $process)
+            gdb -p `pidof $app`;
+        ;;
+        
+        $process_dir)
+            gdb `find $dir -type d -printf '-d %p '` -p `pidof $app`;
+        ;;
+        
+        *)
+            return;
+        ;;
+    esac
 }
